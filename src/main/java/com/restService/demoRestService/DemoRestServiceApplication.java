4eb2f7d5d2b624ac88a11d1d26026c15c8089720
Yusuf <yusufsonmez951@gmail.com>
2023-07-31 21:30:35 +0300

    rest api starter

---
+++ src/main/java/com/restService/demoRestService/DemoRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.restService.demoRestService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restService/demoRestService/DemoRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restService.demoRestService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
